0f72c25df4f27ed175ca29685a4eb3549363c40d
antik <jure@likar.me>
2022-04-07 14:26:11 +0100

    window rules added

--- config.h
+++ config.h
@@ -22,16 +22,18 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1:home", "2:dev", "3:web", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance       title       tags mask     isfloating   monitor */
+	{ "firefox",     NULL,          NULL,       1 << 2,       0,           -1 },
+	{ "Nemo",        NULL,          NULL,       0,            1,           -1 },
+	{ "st-256color", "st-256color", NULL,       1 << 1,       0,           -1 },
+	{ "st-256color", "notes",       NULL,       1,            0,           -1 },
 };
 
 /* layout(s) */
